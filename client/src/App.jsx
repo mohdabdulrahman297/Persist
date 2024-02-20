6d8bed4e988a5c78a3512abe67e803af839882a0
mohdabdulrahman297 <abdulrahmanmohd297@gmail.com>
2024-02-20 22:41:07 +0530

    create the header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import About from "./pages/About";
+import Header from "./components/Header";
+import Recipes from "./pages/Recipes";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recipes" element={<Recipes />} />
       </Routes>
     </BrowserRouter>
   );
