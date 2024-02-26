a213ef3fa4ab603b4b2c4d3ffee7714ae6cb396e
mohdabdulrahman297 <abdulrahmanmohd297@gmail.com>
2024-02-26 08:32:24 +0530

    Create sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Label, TextInput } from "flowbite-react";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* Left */}
+        <div className="flex-1">
+          <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-2 bg-orange-400 rounded-lg text-white">
+              Culina
+            </span>
+            Share
+          </Link>
+          <p className="text-sm mt-5">
+            Join the Culina community and share your favorite recipes with
+            others!
+          </p>
+        </div>
+        {/* Right */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4">
+            <div>
+              <Label value="Your username" />
+              <TextInput type="text" placeholder="Username" id="username" />
+            </div>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="text"
+                placeholder="name@company.com"
+                id="email"
+              />
+            </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput type="text" placeholder="Password" id="password" />
+            </div>
+            <Button
+              className="bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500"
+              type="submit"
+            >
+              Sign Up
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
