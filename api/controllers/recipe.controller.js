2119886a2778c66c689030a38dde99f59606e0f5
mohdabdulrahman297 <abdulrahmanmohd297@gmail.com>
2024-03-01 01:23:27 +0530

    Create get recipes api

--- api/controllers/recipe.controller.js
+++ api/controllers/recipe.controller.js
@@ -18,3 +18,48 @@ export const create = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getRecipes = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const recipes = await Recipe.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.recipeId && { _id: req.query.recipeId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i' } },
+          { content: { $regex: req.query.searchTerm, $options: 'i' } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalRecipes = await Recipe.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthRecipes = await Recipe.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      recipes,
+      totalRecipes,
+      lastMonthRecipes,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/recipe.route.js
+++ api/routes/recipe.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import { create } from '../controllers/recipe.controller.js';
+import { create, getRecipes } from '../controllers/recipe.controller.js';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, create)
+router.post('/create', verifyToken, create);
+router.get('/getRecipes', getRecipes);
 
 
 export default router;
